8441f11d341085f76c81c9dd66c8bc3d9602a3f5
Yu <catfish1106@gmail.com>
2025-08-02 13:43:54 +0800

    seo-better

---
+++ sw.js
@@ -0,0 +1,58 @@
+// Service Worker for RectBlox PWA
+const CACHE_NAME = 'rectblox-v1.0';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/logo.png',
+  '/logo.svg',
+  '/manifest.json'
+];
+
+// Install event
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch event
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Return cached version or fetch from network
+        return response || fetch(event.request);
+      }
+    )
+  );
+});
+
+// Activate event
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Background sync for analytics
+self.addEventListener('sync', function(event) {
+  if (event.tag === 'background-sync') {
+    event.waitUntil(doBackgroundSync());
+  }
+});
+
+function doBackgroundSync() {
+  // Send cached analytics data when online
+  return Promise.resolve();
+}
